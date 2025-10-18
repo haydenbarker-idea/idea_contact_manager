21a6eb59fbe32e5ab262a02a4c68aed92d697b5c
Hayden Barker <hbarker@Ideanetworks.local>
2025-10-18 17:06:45 -0400

    fix: use standalone output mode to fix prerender-manifest.json issue

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Disable static optimization - run as dynamic server
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
